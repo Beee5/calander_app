88075d55e0aae8d4da0db10d15df897e5abd13fe
haeunello <haeun1324@naver.com>
2020-07-04 14:54:27 +0900

    0704

--- screens/Event.js
+++ screens/Event.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
 
 export default function EventScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Event!</Text>
-      </View>
-    );
-  }
+  const data = require("../data.json");
+
+  return (
+    <ScrollView>
+      {data.map((event) => {
+        return <Text key={event.title}>{event.title}</Text>;
+      })}
+    </ScrollView>
+  );
+}
