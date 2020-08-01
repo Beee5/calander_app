ad30adbb9e4906f5b59e64f9ade63537abcac554
haeunello <haeun1324@naver.com>
2020-08-01 11:50:51 +0900

    0801

--- screens/Market.js
+++ screens/Market.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React from "react";
+import { ScrollView, Text, View, Image } from "react-native";
+import { SliderBox } from "react-native-image-slider-box";
 
 export default function MarketScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Market!</Text>
-      </View>
-    );
-  }
+  const data = require("../marketdata.json");
+
+  return (
+    <ScrollView>
+      {data.map((market) => {
+        return (
+          <>
+            <SliderBox images={market.img1} autoplay={true} />
+
+            <Text style={{ fontSize: 24, textAlign: "center", marginTop: 8 }}>
+              {market.title}
+            </Text>
+            <Text
+              style={{ color: "gray", textAlign: "center", marginBottom: 16 }}
+            >
+              상품명: {market.goods_name}
+            </Text>
+            <Text
+              style={{ color: "gray", textAlign: "center", marginBottom: 16 }}
+            >
+              품목: {market.category}
+            </Text>
+            <Text
+              style={{ color: "gray", textAlign: "center", marginBottom: 16 }}
+            >
+              가격: {market.price}
+            </Text>
+            <Text
+              style={{ color: "gray", textAlign: "center", marginBottom: 16 }}
+            >
+              판매자의 한마디: {market.details}
+            </Text>
+          </>
+        );
+      })}
+    </ScrollView>
+  );
+}
